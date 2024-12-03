3f11c2dbe842b909e6e30ab9d1821d1a9e3c9654
nikolalazarevic011 <nikola.lazaa@gmail.com>
2024-12-03 17:03:55 -0600

    18.

--- App.js
+++ App.js
@@ -1,20 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <View>
+                <Text style={styles.dummyText}>Another piece of text</Text>
+            </View>
+            {/* <Text style={{ margin: 16, borderWidth: 6, padding: 16 }}> */}
+            <Text style={styles.dummyText}>Hello</Text>
+            <Button title="Tap Me" />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    dummyText: {
+        margin: 16,
+        borderWidth: 6,
+        padding: 16,
+        borderColor: "red",
+    },
 });
