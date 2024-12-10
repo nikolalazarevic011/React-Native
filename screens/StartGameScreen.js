b39fe27dad2d98576e1288757ef3e501ddb8e10a
nikolalazarevic011 <nikola.lazaa@gmail.com>
2024-12-10 17:03:05 -0600

    end of 57

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -4,9 +4,21 @@ import PrimaryButton from "../components/PrimaryButton";
 function StartGameScreen() {
     return (
         <View style={styles.inputContainer}>
-            <TextInput style={styles.numberInput} maxLength={2} keyboardType="number-pad" autoCapitalize="none" autoCorrect={false}/>
-            <PrimaryButton>Reset</PrimaryButton>
-            <PrimaryButton>Confirm</PrimaryButton>
+            <TextInput
+                style={styles.numberInput}
+                maxLength={2}
+                keyboardType="number-pad"
+                autoCapitalize="none"
+                autoCorrect={false}
+            />
+            <View style={styles.buttonsContainer}>
+                <View style={styles.buttonContainer}>
+                    <PrimaryButton>Reset</PrimaryButton>
+                </View>
+                <View style={styles.buttonContainer}>
+                    <PrimaryButton>Confirm</PrimaryButton>
+                </View>
+            </View>
         </View>
     );
 }
@@ -17,24 +29,31 @@ const styles = StyleSheet.create({
     inputContainer: {
         // flex: 1,
         marginTop: 100,
+        alignItems: "center",
         padding: 16,
-        backgroundColor: "#72063c",
+        backgroundColor: "#4e0329",
         marginTop: 100,
         marginHorizontal: 24,
         borderRadius: 8,
         elevation: 4,
         shadowColor: "black",
-        shadowOffset: {width: 0, height: 2},
+        shadowOffset: { width: 0, height: 2 },
     },
     numberInput: {
         height: 70,
         fontSize: 32,
         width: 50,
-        borderBottomColor: '#ddb52f',
+        borderBottomColor: "#ddb52f",
         borderBottomWidth: 2,
         marginVertical: 8,
-        color:'#ddb52f',
-        fontWeight:"bold",
+        color: "#ddb52f",
+        fontWeight: "bold",
         textAlign: "center",
-    }
+    },
+    buttonsContainer: {
+        flexDirection: "row",
+    },
+    buttonContainer: {
+        flexGrow: 1,
+    },
 });
