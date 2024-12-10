539c833311b9146d2ab7b5332ab3abf5a0433dd1
nikolaLazarevic95 <nikola.lazaa@gmail.com>
2024-12-09 21:19:46 -0600

    sec4

---
+++ screens/StartGameScreen.js
@@ -0,0 +1,12 @@
+import { TextInput, View } from "react-native";
+
+function StartGameScreen() {
+    return (
+        <View>
+            <TextInput />
+            
+        </View>
+    );
+}
+
+export default StartGameScreen;
