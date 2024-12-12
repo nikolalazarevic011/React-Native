2fb36cfa455d1919ad302a6e4f2d23d25fa1d36b
nikolalazarevic011 <nikola.lazaa@gmail.com>
2024-12-12 17:03:12 -0600

    72.done

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,7 +1,40 @@
-import { Text } from 'react-native';
+import { Image, StyleSheet, Text, View } from "react-native";
+import React from "react";
+import Title from "../components/ui/Title";
+import Colors from "../constants/colors";
 
-function GameOverScreen() {
-  return <Text>Game is over!</Text>
+export default function GameOverScreen() {
+    return (
+        <View style={styles.rootContainer}>
+            <Title>Game over </Title>
+            <View style={styles.imageContainer}>
+                <Image
+                    style={styles.image}
+                    source={require("../assets/images/success.png")}
+                />
+            </View>
+        </View>
+    );
 }
 
-export default GameOverScreen;
+const styles = StyleSheet.create({
+    rootContainer: {
+        flex: 1,
+        padding: 24,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    imageContainer: {
+        width: 30,
+        height: 30,
+        borderRadius: 150,
+        borderWidth: 3,
+        borderColor: Colors.primary800,
+        overflow: "hidden",
+        margin: 36,
+    },
+    image: {
+        width: "100%",
+        height: "100%",
+    },
+});
